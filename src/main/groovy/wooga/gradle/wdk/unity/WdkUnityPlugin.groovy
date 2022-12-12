ab8d2fe4dc2f4a71628e31051187f873779af9b0
Christian Sagel <christian.sagel@wooga.net>
2022-12-12 12:31:27 +0100

    Revert "Add task to generate upm packages and to publish them (#43)"
    This reverts commit b8413a618a22fd5d5e5fb7e937454b55579be72e.
    
    # Conflicts:
    #	Jenkinsfile
    #	build.gradle

--- src/main/groovy/wooga/gradle/wdk/unity/WdkUnityPlugin.groovy
+++ src/main/groovy/wooga/gradle/wdk/unity/WdkUnityPlugin.groovy
@@ -17,65 +17,53 @@
 
 package wooga.gradle.wdk.unity
 
+
 import org.apache.commons.io.FileUtils
 import org.gradle.api.Action
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.Task
 import org.gradle.api.artifacts.Configuration
-import org.gradle.api.file.Directory
+import org.sonarqube.gradle.SonarQubeExtension
+import wooga.gradle.dotnetsonar.DotNetSonarqubePlugin
 import org.gradle.api.internal.file.FileResolver
 import org.gradle.api.logging.Logger
 import org.gradle.api.logging.Logging
 import org.gradle.api.plugins.BasePlugin
-import org.gradle.api.provider.Provider
-import org.gradle.api.publish.PublishingExtension
-import org.gradle.api.publish.ivy.IvyPublication
-import org.gradle.api.publish.ivy.plugins.IvyPublishPlugin
-import org.gradle.api.publish.plugins.PublishingPlugin
 import org.gradle.api.tasks.Delete
+import org.gradle.api.tasks.TaskContainer
 import org.gradle.api.tasks.TaskProvider
 import org.gradle.internal.reflect.Instantiator
 import org.gradle.language.base.plugins.LifecycleBasePlugin
-import org.jfrog.gradle.plugin.artifactory.ArtifactoryPlugin
-import org.jfrog.gradle.plugin.artifactory.dsl.ArtifactoryPluginConvention
-import org.jfrog.gradle.plugin.artifactory.dsl.PublisherConfig
-import org.jfrog.gradle.plugin.artifactory.task.ArtifactoryTask
-import org.sonarqube.gradle.SonarQubeExtension
-import wooga.gradle.dotnetsonar.DotNetSonarqubePlugin
 import wooga.gradle.unity.UnityPlugin
 import wooga.gradle.unity.UnityPluginExtension
 import wooga.gradle.unity.UnityTask
-import wooga.gradle.unity.tasks.GenerateUpmPackage
+import wooga.gradle.unity.models.BuildTarget
 import wooga.gradle.unity.tasks.Unity
 import wooga.gradle.wdk.unity.actions.AndroidResourceCopyAction
-import wooga.gradle.wdk.unity.actions.IOSResourceCopyAction
-import wooga.gradle.wdk.unity.actions.WebGLResourceCopyAction
 import wooga.gradle.wdk.unity.config.SonarQubeConfiguration
 import wooga.gradle.wdk.unity.tasks.DefaultResourceCopyTask
+import wooga.gradle.wdk.unity.actions.IOSResourceCopyAction
+import wooga.gradle.wdk.unity.actions.WebGLResourceCopyAction
 
 import javax.inject.Inject
+import java.util.concurrent.Callable
 
 class WdkUnityPlugin implements Plugin<Project> {
 
     static Logger logger = Logging.getLogger(WdkUnityPlugin)
-
-    static String GROUP = "wdk"
-    static String EXTENSION_NAME = "wdk"
 
     static String ASSEMBLE_RESOURCES_TASK_NAME = "assembleResources"
     static String ASSEMBLE_IOS_RESOURCES_TASK_NAME = "assembleIOSResources"
     static String ASSEMBLE_ANDROID_RESOURCES_TASK_NAME = "assembleAndroidResources"
     static String ASSEMBLE_WEBGL_RESOURCES_TASK_NAME = "assembleWebGLResources"
     static String SETUP_TASK_NAME = "setup"
-
+    static String GROUP = "wdk"
+    static String EXTENSION_NAME = "wdk"
     static String ANDROID_RESOURCES_CONFIGURATION_NAME = "android"
     static String IOS_RESOURCES_CONFIGURATION_NAME = "ios"
     static String WEBGL_RESOURCES_CONFIGURATION_NAME = "webgl"
     static String RUNTIME_CONFIGURATION_NAME = "runtime"
-
-    private static String GENERATE_UPM_PACKAGE_TASK_NAME = "upmPack"
-    private static String GENERATE_META_FILES_TASK_NAME = "generateMetaFiles"
 
     static String PERFORM_TEST_BUILD_TASK_NAME = "performTestBuild"
     static String CLEAN_TEST_BUILD_TASK_NAME = "cleanTestBuild"
@@ -99,8 +87,6 @@ class WdkUnityPlugin implements Plugin<Project> {
         project.pluginManager.apply(BasePlugin.class)
         project.pluginManager.apply(UnityPlugin.class)
         project.pluginManager.apply(DotNetSonarqubePlugin.class)
-        project.pluginManager.apply(ArtifactoryPlugin.class)
-        project.pluginManager.apply(IvyPublishPlugin.class)
 
         WdkPluginExtension extension = project.extensions.create(WdkPluginExtension, EXTENSION_NAME, DefaultWdkPluginExtension, project)
 
@@ -112,8 +98,9 @@ class WdkUnityPlugin implements Plugin<Project> {
         configureUnityTaskDependencies(project)
         createTestBuildTasks(project, extension)
         configureSonarqubeTasks(project)
-        configureUpmPublish(project, extension)
     }
+
+
 
     private static void configureExtension(Project project, WdkPluginExtension extension) {
         UnityPluginExtension unity = project.extensions.getByType(UnityPluginExtension)
@@ -127,11 +114,6 @@ class WdkUnityPlugin implements Plugin<Project> {
         extension.iosResourcePluginDir.convention(extension.pluginsDir.dir(WdkUnityPluginConventions.IOS_PLUGIN_DIRECTORY))
         extension.androidResourcePluginDir.convention(extension.pluginsDir.dir(WdkUnityPluginConventions.ANDROID_PLUGIN_DIRECTORY))
         extension.webGLResourcePluginDir.convention(extension.pluginsDir.dir(WdkUnityPluginConventions.WEBGL_PLUGIN_DIRECTORY))
-
-        extension.generateMetaFiles.convention(true)
-        // Try tp deduce the package directory within the Unity assets directories, where the root of the sources of the wdk are
-        // For example: Assets/Wooga/Foobar/*
-        extension.packageDirectory.convention(deducePackageDirectory(project, extension))
     }
 
     private static void addLifecycleTasks(Project project) {
@@ -180,28 +162,28 @@ class WdkUnityPlugin implements Plugin<Project> {
         Configuration webglResources = project.configurations[WEBGL_RESOURCES_CONFIGURATION_NAME]
 
         def iOSResourceCopy = project.tasks.register("assembleIOSResources", DefaultResourceCopyTask,
-            { t ->
-                t.description = "gathers all additional iOS files into the Plugins/iOS directory of the unity project"
-                t.dependsOn(iOSResources)
-                t.resources = iOSResources
-                t.doLast(new IOSResourceCopyAction())
-            })
+                { t ->
+                    t.description = "gathers all additional iOS files into the Plugins/iOS directory of the unity project"
+                    t.dependsOn(iOSResources)
+                    t.resources = iOSResources
+                    t.doLast(new IOSResourceCopyAction())
+                })
 
         def androidResourceCopy = project.tasks.register("assembleAndroidResources", DefaultResourceCopyTask,
-            { t ->
-                t.description = "gathers all *.jar and AndroidManifest.xml files into the Plugins/Android directory of the unity project"
-                t.dependsOn(androidResources)
-                t.resources androidResources
-                t.doLast(new AndroidResourceCopyAction())
-            })
+                { t ->
+                    t.description = "gathers all *.jar and AndroidManifest.xml files into the Plugins/Android directory of the unity project"
+                    t.dependsOn(androidResources)
+                    t.resources androidResources
+                    t.doLast(new AndroidResourceCopyAction())
+                })
 
         def webglResourceCopy = project.tasks.register("assembleWebGLResources", DefaultResourceCopyTask,
-            { t ->
-                t.description = "gathers all webgl related files into the Plugins/webGL directory of the unity project"
-                t.dependsOn(webglResources)
-                t.resources webglResources
-                t.doLast(new WebGLResourceCopyAction())
-            })
+                { t ->
+                    t.description = "gathers all webgl related files into the Plugins/webGL directory of the unity project"
+                    t.dependsOn(webglResources)
+                    t.resources webglResources
+                    t.doLast(new WebGLResourceCopyAction())
+                })
 
         def assembleTask = project.tasks[ASSEMBLE_RESOURCES_TASK_NAME]
         assembleTask.dependsOn iOSResourceCopy, androidResourceCopy, webglResourceCopy
@@ -323,95 +305,5 @@ class WdkUnityPlugin implements Plugin<Project> {
             buildTaskName = SONARQUBE_BUILD_TASK_NAME
             return it
         }.configure(unityExt, sonarExt)
-    }
-
-    private static void configureUpmPublish(Project project, WdkPluginExtension extension) {
-
-        def unityExtension = project.extensions.getByType(UnityPluginExtension)
-        def group = "UPM"
-
-        // Create the tasks
-        def upmGenerateMetaFiles = project.tasks.register(GENERATE_META_FILES_TASK_NAME, Unity) {
-            it.group = group
-        }
-        def upmPack = project.tasks.register(GENERATE_UPM_PACKAGE_TASK_NAME, GenerateUpmPackage) {
-            if (extension.generateMetaFiles.present && extension.generateMetaFiles.get()) {
-                it.dependsOn(upmGenerateMetaFiles)
-            }
-            it.group = group
-            it.packageDirectory.convention(extension.packageDirectory)
-        }
-
-        // Create the configuration
-        def config = project.configurations.maybeCreate(WdkUnityPluginConventions.upmConfigurationName)
-        config.transitive = false
-        def upmArtifact = project.artifacts.add(WdkUnityPluginConventions.upmConfigurationName, upmPack)
-
-        // Create the publication object
-        def publishing = project.extensions.getByType(PublishingExtension)
-        def upmPublication = publishing.publications.maybeCreate(WdkUnityPluginConventions.upmPublicationName, IvyPublication)
-        // TODO: Try to adjust? It has to be evaluated later
-        project.afterEvaluate {
-            upmPublication.setModule(upmPack.flatMap({ it.packageName }).getOrNull())
-        }
-
-        upmPublication.artifact(upmArtifact)
-
-        // Configure the artifactory plugin
-        // Some properties, such as 'contextUrl', `publish.repository.repoKey|username|password` have to be configured by the user
-        def artifactory = (ArtifactoryPluginConvention) project.convention.plugins.get("artifactory")
-        artifactory.publish { PublisherConfig publisherConfig ->
-            publisherConfig.repository { repo ->
-                repo.ivy { PublisherConfig.Repository it ->
-                    it.artifactLayout = '[module]/-/[module]-[revision].[ext]'
-                    it.mavenCompatible = false
-                }
-            }
-        }
-
-        project.tasks.withType(ArtifactoryTask).configureEach({ defaultTask ->
-            defaultTask.publications(WdkUnityPluginConventions.upmPublicationName)
-            defaultTask.publishArtifacts = false
-            defaultTask.publishIvy = false
-        })
-
-        // Configure the publishing task dependencies
-        def artifactoryPublishTask = project.tasks.getByName("artifactoryPublish")
-        def publishTask = project.tasks.getByName(PublishingPlugin.PUBLISH_LIFECYCLE_TASK_NAME)
-        publishTask.dependsOn(artifactoryPublishTask)
-
-        if (project.rootProject != project) {
-            def rootPublishTask = project.rootProject.tasks.getByName(PublishingPlugin.PUBLISH_LIFECYCLE_TASK_NAME)
-            rootPublishTask.dependsOn(publishTask)
-        }
-    }
-
-    private static Provider<Directory> deducePackageDirectory(Project project, WdkPluginExtension extension) {
-        project.provider({
-            Directory result = null
-            // Start at $Project/Assets/Wooga
-            if (extension.assetsDir.present) {
-                def woogaDir = extension.assetsDir.get()
-                // Search for a directory that contains a package.json file
-                def tree = woogaDir.asFileTree
-                def manifestFiles = tree.filter({ File file -> file.name == "package.json" })
-                switch (manifestFiles.size()){
-                    case 1:
-                        def manifest = manifestFiles.first()
-                        result = woogaDir.dir(manifest.parentFile.path)
-                        break
-                    case 0:
-                        logger.warn("No package manifest files (package.json) were found")
-                        break
-                    default:
-                        logger.warn("More then one package manifest file (package.json) was found")
-                        break
-                }
-            }
-            if (result == null) {
-                logger.warn("Could not deduce the package directory for this wdk")
-            }
-            result
-        })
     }
 }
