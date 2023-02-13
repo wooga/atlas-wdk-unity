5621b7c70da0e31267acc22d9860e67b6a193e23
Robin Hentschel <robin.hentschel@wooga.net>
2023-02-13 12:30:04 +0100

    chang manifest file to be optional

--- src/main/groovy/wooga/gradle/wdk/unity/tasks/ResolveUnityPackages.groovy
+++ src/main/groovy/wooga/gradle/wdk/unity/tasks/ResolveUnityPackages.groovy
@@ -1,17 +1,27 @@
 package wooga.gradle.wdk.unity.tasks
 
+import org.gradle.api.file.FileCollection
 import org.gradle.api.file.RegularFile
 import org.gradle.api.file.RegularFileProperty
 import org.gradle.api.provider.Provider
-import org.gradle.api.tasks.InputFile
+import org.gradle.api.tasks.InputFiles
+import org.gradle.api.tasks.Internal
 import org.gradle.api.tasks.OutputFile
+import org.gradle.api.tasks.SkipWhenEmpty
 import org.gradle.api.tasks.TaskAction
 import wooga.gradle.unity.UnityTask
 
 class ResolveUnityPackages extends UnityTask {
+
+    @InputFiles
+    @SkipWhenEmpty
+    FileCollection getInputFiles() {
+        project.files(manifest)
+    }
+
     private final RegularFileProperty manifest = objects.fileProperty()
 
-    @InputFile
+    @Internal
     RegularFileProperty getManifest() {
         manifest
     }
